ada0e55ae6b6f2e2028aa07c6b3980a475d2b376
Aleksandr Kozhushkevich <kazhuha@gmail.com>
2022-11-20 18:23:03 +0300

    lesson 2

--- flsite.py
+++ flsite.py
@@ -1,16 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+menu = ['Установка', 'Первое приложение', 'Обратная связь']
 
 
 @app.route('/')
 def index():
-    return 'index'
+    return render_template('index.html', menu=menu)
 
 
 @app.route('/about')
 def about():
-    return '<h1>О сайте</h1>'
+    title = 'О сайте'
+    return render_template('about.html', title=title, menu=menu)
 
 
 if __name__ == '__main__':
